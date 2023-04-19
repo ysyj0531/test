e36490bfa84ef1dee4990148b1869311af43a1cf
Mr.DAO <113940100+ysyj0531@users.noreply.github.com>
2023-04-19 15:12:11 +0800

    Update Shardeum-install.sh

--- Shardeum-install.sh
+++ Shardeum-install.sh
@@ -28,10 +28,6 @@ sudo yum install -y docker-ce docker-ce-cli containerd.io
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-# 验证Docker引擎的安装是否成功，运行hello-world
-sudo systemctl start docker
-sudo docker run hello-world
-
 # 检查docker版本
 docker -v
 docker-compose -v
